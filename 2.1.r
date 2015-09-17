7e8c7a1a05128234f9084fac76c8be0b9ea3fd30
Arco Dielhof <Veltrex@live.nl>
2015-09-17 19:51:24 +0200

    Opdracht 2.1 en 2.2 (+ formules Combinatoriek)

--- 2.1.r
+++ 2.1.r
@@ -1,16 +1,17 @@
-#1A
-#Er zijn 10 soorten van iets om uit te kiezen.
-aantalSoorten <- 10
-#Er mogen 5 verschillende dingen worden gekozen
-aantalTeKiezenSoorten <- 5
-#Er zijn 10! aantal erschillende volgordes om de keuzes in te zetten.
-possibleOrders <- factorial(aantalSoorten)
-# er zijn 10! / (10-5)! verschillende mogelijkheden om de spullen te plaatsen.
-combinationCount <- possibleOrders / factorial(aantalSoorten - aantalTeKiezenSoorten)
-print(paste("Aantal mogelijke combinaties om vijf verschillende soorten fruit in een zakje te stoppen:", format(combinationCount)))
+#1a
+nValue1a <- 10
+kValue1a <- 5
+answer1a <- factorial(nValue1a - 1 + kValue1a) / (factorial(kValue1a) * factorial(nValue1a - 1))
+print(paste("1a = ", format(answer1a)))
 
-#1B
-students <- 25
-chairs <- 30
-studentChairCombinations <- format(factorial(chairs) / factorial(chairs - students))
-print(paste("Aantal mogelijke combinaties om studenten over de stoelen te verdelen:", studentChairCombinations))
+#1b
+nValue1b <- 30
+kValue1b <- 25
+answer1b <- factorial(nValue1b) / (factorial(kValue1b) * factorial(nValue1b - kValue1b))
+print(paste("1b = ", format(answer1b)))
+
+#1c
+nValue1c <- 6
+kValue1c <- 5
+answer1c <- nValue1c^kValue1c
+print(paste("1c = ", format(answer1c)))
---
+++ 2.2.r
@@ -0,0 +1,35 @@
+#2a
+nValue2a <- 2*30
+kValue2a <- 20+15
+answer2a <- factorial(nValue2a) / factorial(nValue2a - kValue2a)
+print(paste("2a = ", format(answer2a)))
+
+#2b
+nValue2b <- 2*30
+kValue2b <- 20+15
+answer2b <- factorial(nValue2b) / (factorial(kValue2b) * factorial(nValue2b - kValue2b))
+print(paste("2b = ", format(answer2b)))
+
+#2c
+n1Value2c <- 30
+n2Value2c <- 30
+k1Value2c <- 20
+k2Value2c <- 15
+answer2c <- (factorial(n1Value2c) / (factorial(k1Value2c) * factorial(n1Value2c - k1Value2c))) + (factorial(n2Value2c) / (factorial(k2Value2c) * factorial(n2Value2c - k2Value2c))) 
+print(paste("2c = ", format(answer2c)))
+
+#2d
+n1Value2d <- 30
+n2Value2d <- 30
+k1Value2d <- 35
+k2Value2d <- k1Value2d - n1Value2d
+answer2d <- (factorial(k1Value2d) - factorial(k2Value2d)) * (factorial(n2Value2d) / factorial(n2Value2d - k2Value2d))
+print(paste("2d = ", format(answer2d)))
+
+#2e
+n1Value2e <- 25
+n2Value2e <- 30
+k1Value2e <- 30
+k2Value2e <- k1Value2e - n1Value2e
+answer2e <- factorial(n1Value2e) * (factorial(k1Value2e) - factorial(k2Value2e)) * (factorial(n2Value2e) / factorial(n2Value2e - k2Value2e))
+print(paste("2e = ", format(answer2e)))
