cf343af8418c8f8ca86b3e54e05ea8a8be189d2e
Arco Dielhof <Veltrex@live.nl>
2015-09-30 12:48:07 +0200

    opdr 2.1 & 2.2

--- 2.1.r
+++ 2.1.r
@@ -1,16 +1,16 @@
-#1a
+#2.1a
 nValue1a <- 10
 kValue1a <- 5
 answer1a <- factorial(nValue1a - 1 + kValue1a) / (factorial(kValue1a) * factorial(nValue1a - 1))
 print(paste("1a = ", format(answer1a)))
 
-#1b
+#2.1b
 nValue1b <- 30
 kValue1b <- 25
 answer1b <- factorial(nValue1b) / (factorial(kValue1b) * factorial(nValue1b - kValue1b))
 print(paste("1b = ", format(answer1b)))
 
-#1c
+#2.1c
 nValue1c <- 6
 kValue1c <- 5
 answer1c <- nValue1c^kValue1c
--- 2.2.r
+++ 2.2.r
@@ -1,16 +1,16 @@
-#2a
+#2.2a
 nValue2a <- 2*30
 kValue2a <- 20+15
 answer2a <- factorial(nValue2a) / factorial(nValue2a - kValue2a)
 print(paste("2a = ", format(answer2a)))
 
-#2b
+#2.2b
 nValue2b <- 2*30
 kValue2b <- 20+15
 answer2b <- factorial(nValue2b) / (factorial(kValue2b) * factorial(nValue2b - kValue2b))
 print(paste("2b = ", format(answer2b)))
 
-#2c
+#2.2c
 n1Value2c <- 30
 n2Value2c <- 30
 k1Value2c <- 20
@@ -18,7 +18,7 @@ k2Value2c <- 15
 answer2c <- (factorial(n1Value2c) / (factorial(k1Value2c) * factorial(n1Value2c - k1Value2c))) + (factorial(n2Value2c) / (factorial(k2Value2c) * factorial(n2Value2c - k2Value2c))) 
 print(paste("2c = ", format(answer2c)))
 
-#2d
+#2.2d
 n1Value2d <- 30
 n2Value2d <- 30
 k1Value2d <- 35
@@ -26,7 +26,7 @@ k2Value2d <- k1Value2d - n1Value2d
 answer2d <- (factorial(k1Value2d) - factorial(k2Value2d)) * (factorial(n2Value2d) / factorial(n2Value2d - k2Value2d))
 print(paste("2d = ", format(answer2d)))
 
-#2e
+#2.2e
 n1Value2e <- 25
 n2Value2e <- 30
 k1Value2e <- 30
